3a8fd0ae7f99f3f41cee0256b225de3f669e7632
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-12-05 15:49:31 +0700

    feat: add function to extract commands from file

---
+++ internal/file.go
@@ -0,0 +1,28 @@
+package internal
+
+import (
+	"bufio"
+	"os"
+)
+
+func ExtractCommandsFromFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var commands []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		command := scanner.Text()
+		commands = append(commands, command)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return commands, nil
+}
