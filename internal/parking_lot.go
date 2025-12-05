b5d4345755ea908b13d0d110ae15908cb774a353
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-12-05 17:52:37 +0700

    feat: add `ParkingLot` implementation (utilizes `ParkingSlot` and `ParkingHeap`)

---
+++ internal/parking_lot.go
@@ -0,0 +1,88 @@
+package internal
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type ParkingLot struct {
+	availableSlots *ParkingHeap
+	occupiedSlots  map[string]*ParkingSlot
+	capacity       int
+}
+
+func NewParkingLot(capacity int) (*ParkingLot, error) {
+	if capacity <= 0 {
+		return nil, fmt.Errorf("capacity must be positive")
+	}
+
+	parkingHeap := &ParkingHeap{}
+	heap.Init(parkingHeap)
+
+	for i := range capacity {
+		parkingSlot := &ParkingSlot{
+			ID:                i + 1,
+			DistanceFromEntry: i + 1,
+			Available:         true,
+			CarNumber:         "",
+		}
+		heap.Push(parkingHeap, parkingSlot)
+	}
+
+	return &ParkingLot{
+		availableSlots: parkingHeap,
+		occupiedSlots:  map[string]*ParkingSlot{},
+		capacity:       capacity,
+	}, nil
+}
+
+func (pl *ParkingLot) IsEmpty() bool {
+	return pl.availableSlots.Len() == pl.capacity
+}
+
+func (pl *ParkingLot) IsFull() bool {
+	return pl.availableSlots.Len() == 0
+}
+
+func (pl *ParkingLot) ParkCar(carNumber string) (*ParkingSlot, error) {
+	if pl.IsFull() {
+		return nil, fmt.Errorf("sorry, parking lot is full")
+	}
+
+	if parkingSlot, ok := pl.occupiedSlots[carNumber]; ok {
+		return nil, fmt.Errorf(
+			"registration number %s is already parked in Slot Number %d",
+			parkingSlot.CarNumber, parkingSlot.ID,
+		)
+	}
+
+	parkingSlot, ok := heap.Pop(pl.availableSlots).(*ParkingSlot)
+	if !ok {
+		return nil, fmt.Errorf("ParkingSlot type assertion failed")
+	}
+
+	parkingSlot.Available = false
+	parkingSlot.CarNumber = carNumber
+	pl.occupiedSlots[carNumber] = parkingSlot
+
+	return parkingSlot, nil
+}
+
+func (pl *ParkingLot) RemoveCar(carNumber string) (*ParkingSlot, error) {
+	if pl.IsEmpty() {
+		return nil, fmt.Errorf("sorry, parking lot is empty")
+	}
+
+	parkingSlot, ok := pl.occupiedSlots[carNumber]
+	if !ok {
+		return nil, fmt.Errorf("registration number %s not found", parkingSlot.CarNumber)
+	}
+
+	heap.Push(pl.availableSlots, parkingSlot)
+
+	parkingSlot.Available = true
+	parkingSlot.CarNumber = ""
+	delete(pl.occupiedSlots, carNumber)
+
+	return parkingSlot, nil
+}
