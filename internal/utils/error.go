151e067cae63df8409e0e8428297d53399c839d9
ahmadnaufalhakim <ahmadnaufalhakim@gmail.com>
2025-12-06 05:34:46 +0700

    fix: add check if `errMsg` is an empty string

--- internal/utils/error.go
+++ internal/utils/error.go
@@ -7,6 +7,11 @@ import (
 
 func PrintCapitalizedError(err error) {
 	errMsg := err.Error()
+	if len(errMsg) == 0 {
+		fmt.Println()
+		return
+	}
+
 	errMsg = strings.ToUpper(errMsg[:1]) + errMsg[1:]
 	fmt.Println(errMsg)
 }
